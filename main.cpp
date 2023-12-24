a3e083a6aa3edf099eebc8b6fe815946749ea236
jasper baijens <jasperbayens@outlook.com>
2023-12-24 02:11:22 +0100

    Setup project and added glad

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by User on 12/24/2023.
+//
+
+#include "src/Testing.hpp"
+
+int main() {
+    Testing t;
+    t.test();
+}
---
+++ src/Testing.cpp
@@ -0,0 +1,11 @@
+//
+// Created by User on 12/24/2023.
+//
+
+#include "Testing.hpp"
+
+#include <iostream>
+
+void Testing::test() {
+    printf("Hello world");
+}
